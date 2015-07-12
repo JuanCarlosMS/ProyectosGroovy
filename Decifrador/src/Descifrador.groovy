6b3b961d395a36511d014a7bea7880c54bdc9bcd
JuanCarlosMS <jmolina@sisntelti>
2015-07-12 16:18:40 -0500

    Ya descifra el mensaje pero solo ese mensaje

--- Decifrador/src/Descifrador.groovy
+++ Decifrador/src/Descifrador.groovy
@@ -10,6 +10,7 @@ class LeerArchivo{
     int contadorene = 0;
 
     def texto = """"""
+    def textoTraducido = """"""
 
     int caracter
 
@@ -31,39 +32,39 @@ class LeerArchivo{
             //println "Caracater:${caracter}"
             if( caracter >= 65 && caracter <= 90 )
             {
-                //texto = texto + "ñ"
                 contarLetras(caracter + 32)
-                println "$caracter"
+                textoTraducido = textoTraducido + (char)caracter
+
             } else if( caracter >= 97 && caracter <= 122 ){
-                //texto = texto + (char)caracter
+
                 contarLetras(caracter)
+                textoTraducido = textoTraducido + (char)caracter
+
             }else if(caracter == 65533){
                 contadorene++
+
+                textoTraducido = textoTraducido + (char)caracter
+                //almacena el texto traducido
+            }else{
+                //almacena el texto traducido
+                textoTraducido = textoTraducido + (char)caracter
             }
 
         }
-        //imprimirTexto()
-        imprimrlista()
-        imprimirOrdenada()
+
+        imprimirTextoTraducido(textoTraducido)
+        //prueba()
 
     }// fin del constructor
+
+
 
     void contarLetras(int caracter)
     {
         def contador = 1;
         caracteresMin[caracter] = caracteresMin[caracter] + contador
     }
-
-    def imprimirTexto = {
-        println texto.toLowerCase()
-    }
-
-    def inciar = {
-        for(int i = 97; i <= 122; i++)
-        {
-            caracteresMin[i] = 0
-        }
-    }
+    
 
     def imprimrlista = {
         for(int i = 97; i <= 122; i++)
@@ -77,6 +78,155 @@ class LeerArchivo{
         caracteresMin.sort().each {println it}
     }
 
+    void imprimirTextoTraducido(String textoEntrada)
+    {
+        def arreglo = []
+        for (int i = 0; i < textoEntrada.length(); i++)
+        {
+            arreglo.add(textoEntrada.charAt(i))
+        }
+
+        for (int i = 0; i < arreglo.size(); i ++)
+        {
+            arreglo[i] = traducirCaracter(arreglo[i])
+        }
+
+        //imprime
+
+        for (int i = 0; i < arreglo.size(); i ++)
+        {
+            print arreglo[i]
+        }
+
+    }
+
+    String traducirCaracter(Character letra)
+    {
+        String letraCambiada
+        switch (letra)
+        {
+            case 'a':
+            case 'A':
+                letraCambiada = 'u'
+                break;
+            case 'b':
+            case 'B':
+                letraCambiada = 'v'
+                break;
+            case 'c':
+            case 'C':
+                letraCambiada = 'w'
+                break;
+            case 'd':
+            case 'D':
+                letraCambiada = 'x'
+                break;
+            case 'e':
+            case 'E':
+                letraCambiada = 'y'
+                break;
+            case 'f':
+            case 'F':
+                letraCambiada = 'z'
+                break;
+            case 'g':
+            case 'G':
+                letraCambiada = 'a'
+                break;
+            case 'h':
+            case 'H':
+                letraCambiada = 'b'
+                break;
+            case 'i':
+            case 'I':
+                letraCambiada = 'c'
+                break;
+            case 'j':
+            case 'J':
+                letraCambiada = 'd'
+                break;
+            case 'k':
+            case 'K':
+                letraCambiada = 'e'
+                break;
+            case 'l':
+            case 'L':
+                letraCambiada = 'f'
+                break;
+            case 'm':
+            case 'M':
+                letraCambiada = 'g'
+                break;
+            case 'n':
+            case 'N':
+                letraCambiada = 'h'
+                break;
+            case '�':
+            case '�':
+                letraCambiada = 'i'
+                break;
+            case 'o':
+            case 'O':
+                letraCambiada = 'j'
+                break;
+            case 'p':
+            case 'P':
+                letraCambiada = 'k'
+                break;
+            case 'q':
+            case 'Q':
+                letraCambiada = 'l'
+                break;
+            case 'r':
+            case 'R':
+                letraCambiada = 'm'
+                break;
+            case 's':
+            case 'S':
+                letraCambiada = 'n'
+                break;
+            case 't':
+            case 'T':
+                letraCambiada = 'ñ'
+                break;
+            case 'u':
+            case 'U':
+                letraCambiada = 'o'
+                break;
+            case 'v':
+            case 'V':
+                letraCambiada = 'p'
+                break;
+            case 'w':
+            case 'W':
+                letraCambiada = 'q'
+                break;
+            case 'x':
+            case 'X':
+                letraCambiada = 'r'
+                break;
+            case 'y':
+            case 'Y':
+                letraCambiada = 's'
+                break;
+            case 'z':
+            case 'Z':
+                letraCambiada = 't'
+                break;
+
+
+            default:
+                letraCambiada = letra;
+
+
+        }
+
+        return letraCambiada;
+    }
+
+
+
 }
 
-new LeerArchivo("C:/Prueba/encirp2.txt")
+
+new LeerArchivo("C:/Users/JC/Documents/Codigo.txt")
