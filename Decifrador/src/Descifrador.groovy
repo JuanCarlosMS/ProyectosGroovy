0637863e7419805cd7f606f28ab0685e7234023e
JuanCarlosMS <jmolina@sisntelti>
2015-07-13 01:29:11 -0500

    Decifrador actualizado, se removio Switch-case por una List sigue desifrando nada mas el texto dado

--- Decifrador/src/Descifrador.groovy
+++ Decifrador/src/Descifrador.groovy
@@ -6,7 +6,7 @@ class LeerArchivo{
     FileReader fr;
 
     def caracteresMin = []
-    def caracteresMay = []
+
     int contadorene = 0;
 
     def texto = """"""
@@ -14,219 +14,120 @@ class LeerArchivo{
 
     int caracter
 
+    String cadenaEjemplo = "Ynkxquip"
+    def mapaAbecedario = ['a':'u', 'b':'v', 'c':'w', 'd':'x', 'e':'y', 'f':'z', 'g':'a', 'h':'b','i':'c', 'j':'d', 'k':'e', 'l':'f',
+                          'm':'g', 'n':'h', '65533':'i', 'o':'j','p':'k', 'q':'l', 'r':'m', 's':'n', 't':'ñ', 'u':'o', 'v':'p', 'w':'q',
+                          'x':'r', 'y':'s', 'z':'t']
+
 
     //constructor
     LeerArchivo(nombre) {
 
         fr = new FileReader(nombre)
         caracter = fr.read();
-        inciar()
+        iniciar()
         contarLetras(caracter+32)
         texto = texto + (char)caracter
+        textoTraducido = textoTraducido + (char)caracter
         texto.toLowerCase()
+
+        //recorre caracter por caracter del texto hasta su final
         while( caracter != -1 )
         {
             caracter = fr.read()
             //String letra = "" + caracter
 
             //println "Caracater:${caracter}"
+            //para las minusculas
             if( caracter >= 65 && caracter <= 90 )
             {
                 contarLetras(caracter + 32)
                 textoTraducido = textoTraducido + (char)caracter
 
-            } else if( caracter >= 97 && caracter <= 122 ){
+            } else if( caracter >= 97 && caracter <= 122 ){//para las mayusculas
 
                 contarLetras(caracter)
                 textoTraducido = textoTraducido + (char)caracter
 
-            }else if(caracter == 65533){
+            }else if(caracter == 65533){//para la ñ
                 contadorene++
 
                 textoTraducido = textoTraducido + (char)caracter
                 //almacena el texto traducido
-            }else{
+            }else{//para el resto del texot
                 //almacena el texto traducido
                 textoTraducido = textoTraducido + (char)caracter
             }
 
-        }
+        }//fin del while
 
-        imprimirTextoTraducido(textoTraducido)
+        //imprimirTextoTraducido(textoTraducido)
         //prueba()
+        //def llave = ""+cadenaEjemplo.charAt(1)
+        //println mapaAbecedario.containsKey(llave)
+        traducirTexto(textoTraducido)
+
 
     }// fin del constructor
 
 
+    //cambiar la letra del texto por la que esta contenida en la list mapaAbecedario
+    void traducirTexto(String frase)
+    {
+        def fraseT = []
+        for (int i = 0; i < frase.length(); i++)
+        {
+            def llave = ""+frase.charAt(i)
+            if(mapaAbecedario.containsKey(llave.toLowerCase()))
+            {
+                //fraseT[i] = fraseT[i] + mapaAbecedario.get(llave)
+                fraseT.add(mapaAbecedario.get(llave.toLowerCase()))
+            }else{
+                fraseT.add(frase.charAt(i))
+            }
+            //println frase.charAt(i)
+        }
+        //imprimer el arreglo
+        for (int i = 0; i < fraseT.size(); i ++)
+        {
+            print fraseT[i]
+        }
+    }// fin de traducirTexto
 
+
+    //incializa el arreclo caracteresMin en ''
+    void iniciar()
+    {
+        for (int i = 97; i <= 122; i++ )
+        {
+            caracteresMin[i] = ''
+        }
+    }// fin de inciar
+
+    //cuenta el total de letras del abecedario que hay en el texto
     void contarLetras(int caracter)
     {
         def contador = 1;
         caracteresMin[caracter] = caracteresMin[caracter] + contador
-    }
-    
+    }//fin de contarLetras
 
+
+    //closure que imprime los elementos contenidos en el arreglo caracterMin
     def imprimrlista = {
         for(int i = 97; i <= 122; i++)
         {
             println "Caracter ${(char)i}: ${caracteresMin[i]}"
         }
         println "Caracter ñ: ${contadorene}"
-    }
+    }//fin de imprimir lista
 
+    //imprime el arreglo ordenado de menor a mayo
     def imprimirOrdenada = {
         caracteresMin.sort().each {println it}
-    }
-
-    void imprimirTextoTraducido(String textoEntrada)
-    {
-        def arreglo = []
-        for (int i = 0; i < textoEntrada.length(); i++)
-        {
-            arreglo.add(textoEntrada.charAt(i))
-        }
-
-        for (int i = 0; i < arreglo.size(); i ++)
-        {
-            arreglo[i] = traducirCaracter(arreglo[i])
-        }
-
-        //imprime
-
-        for (int i = 0; i < arreglo.size(); i ++)
-        {
-            print arreglo[i]
-        }
-
-    }
-
-    String traducirCaracter(Character letra)
-    {
-        String letraCambiada
-        switch (letra)
-        {
-            case 'a':
-            case 'A':
-                letraCambiada = 'u'
-                break;
-            case 'b':
-            case 'B':
-                letraCambiada = 'v'
-                break;
-            case 'c':
-            case 'C':
-                letraCambiada = 'w'
-                break;
-            case 'd':
-            case 'D':
-                letraCambiada = 'x'
-                break;
-            case 'e':
-            case 'E':
-                letraCambiada = 'y'
-                break;
-            case 'f':
-            case 'F':
-                letraCambiada = 'z'
-                break;
-            case 'g':
-            case 'G':
-                letraCambiada = 'a'
-                break;
-            case 'h':
-            case 'H':
-                letraCambiada = 'b'
-                break;
-            case 'i':
-            case 'I':
-                letraCambiada = 'c'
-                break;
-            case 'j':
-            case 'J':
-                letraCambiada = 'd'
-                break;
-            case 'k':
-            case 'K':
-                letraCambiada = 'e'
-                break;
-            case 'l':
-            case 'L':
-                letraCambiada = 'f'
-                break;
-            case 'm':
-            case 'M':
-                letraCambiada = 'g'
-                break;
-            case 'n':
-            case 'N':
-                letraCambiada = 'h'
-                break;
-            case '�':
-            case '�':
-                letraCambiada = 'i'
-                break;
-            case 'o':
-            case 'O':
-                letraCambiada = 'j'
-                break;
-            case 'p':
-            case 'P':
-                letraCambiada = 'k'
-                break;
-            case 'q':
-            case 'Q':
-                letraCambiada = 'l'
-                break;
-            case 'r':
-            case 'R':
-                letraCambiada = 'm'
-                break;
-            case 's':
-            case 'S':
-                letraCambiada = 'n'
-                break;
-            case 't':
-            case 'T':
-                letraCambiada = 'ñ'
-                break;
-            case 'u':
-            case 'U':
-                letraCambiada = 'o'
-                break;
-            case 'v':
-            case 'V':
-                letraCambiada = 'p'
-                break;
-            case 'w':
-            case 'W':
-                letraCambiada = 'q'
-                break;
-            case 'x':
-            case 'X':
-                letraCambiada = 'r'
-                break;
-            case 'y':
-            case 'Y':
-                letraCambiada = 's'
-                break;
-            case 'z':
-            case 'Z':
-                letraCambiada = 't'
-                break;
+    }//fin de closure imprimirOrdenada
 
 
-            default:
-                letraCambiada = letra;
-
-
-        }
-
-        return letraCambiada;
-    }
-
-
-
-}
+}//fin de la clase
 
 
 new LeerArchivo("C:/Users/JC/Documents/Codigo.txt")
